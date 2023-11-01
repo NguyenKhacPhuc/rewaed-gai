44a5b59d471ee89dfb290117ef68ef1fd68dec57
PhucNK1 <nguyenkhacphuc311@gmail.com>
2023-11-01 23:57:42 +0700

    add image detector package pt5

--- app/src/main/java/com/tcp/rewaed/ui/textdetector/TextRecognitionProcessor.kt
+++ app/src/main/java/com/tcp/rewaed/ui/textdetector/TextRecognitionProcessor.kt
@@ -8,6 +8,7 @@ import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.text.TextRecognizerOptionsInterface
+import timber.log.Timber
 
 /** Processor for the text detector demo. */
 class TextRecognitionProcessor(
@@ -30,7 +31,7 @@ class TextRecognitionProcessor(
   }
 
   override fun onSuccess(text: Text, graphicOverlay: GraphicOverlay) {
-    Log.d(TAG, "On-device Text detection successful")
+    Timber.tag(TAG).d("On-device Text detection successful")
     logExtrasForTesting(text)
     graphicOverlay.add(
       TextGraphic(
@@ -44,42 +45,34 @@ class TextRecognitionProcessor(
   }
 
   override fun onFailure(e: Exception) {
-    Log.w(TAG, "Text detection failed.$e")
+    Timber.tag(TAG).w("Text detection failed." + e)
   }
 
   companion object {
     private const val TAG = "TextRecProcessor"
     private fun logExtrasForTesting(text: Text?) {
       if (text != null) {
-        Log.v(MANUAL_TESTING_LOG, "Detected text has : " + text.textBlocks.size + " blocks")
+        Timber.tag(MANUAL_TESTING_LOG).v("Detected text has : " + text.textBlocks.size + " blocks")
         for (i in text.textBlocks.indices) {
           val lines = text.textBlocks[i].lines
-          Log.v(
-            MANUAL_TESTING_LOG,
-            String.format("Detected text block %d has %d lines", i, lines.size)
-          )
+          Timber.tag(MANUAL_TESTING_LOG)
+            .v(String.format("Detected text block %d has %d lines", i, lines.size))
           for (j in lines.indices) {
             val elements = lines[j].elements
-            Log.v(
-              MANUAL_TESTING_LOG,
-              String.format("Detected text line %d has %d elements", j, elements.size)
-            )
+            Timber.tag(MANUAL_TESTING_LOG)
+              .v(String.format("Detected text line %d has %d elements", j, elements.size))
             for (k in elements.indices) {
               val element = elements[k]
-              Log.v(
-                MANUAL_TESTING_LOG,
-                String.format("Detected text element %d says: %s", k, element.text)
-              )
-              Log.v(
-                MANUAL_TESTING_LOG,
+              Timber.tag(MANUAL_TESTING_LOG)
+                .v(String.format("Detected text element %d says: %s", k, element.text))
+              Timber.tag(MANUAL_TESTING_LOG).v(
                 String.format(
                   "Detected text element %d has a bounding box: %s",
                   k,
                   element.boundingBox!!.flattenToString()
                 )
               )
-              Log.v(
-                MANUAL_TESTING_LOG,
+              Timber.tag(MANUAL_TESTING_LOG).v(
                 String.format(
                   "Expected corner point size is 4, get %d",
                   element.cornerPoints!!.size
